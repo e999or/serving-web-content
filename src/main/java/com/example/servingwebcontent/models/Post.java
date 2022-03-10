fb9c174451908c939139713dfccb1d677272275e
Eg <e999or@bk.ru>
2022-03-10 21:49:48 +0300

    new dependency

---
+++ src/main/java/com/example/servingwebcontent/models/Post.java
@@ -0,0 +1,50 @@
+package com.example.servingwebcontent.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title, text;
+    private int vies;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getVies() {
+        return vies;
+    }
+
+    public void setVies(int vies) {
+        this.vies = vies;
+    }
+}
